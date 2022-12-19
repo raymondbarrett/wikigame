484872439c36382de1028e92f927b52ecd77d423
Raymond Barrett <rbarrett38@gatech.edu>
2022-12-19 09:48:07 -0500

    fixes for backend

--- backend.py
+++ backend.py
@@ -18,20 +18,40 @@ print(wiki.status_code)
 soup = BeautifulSoup(wiki.text, 'html.parser')
 match_elements = soup.find_all(re.compile('(li)|(h2)'))
 
-incl = False
-clues_text = []
+curr_sec = ''
+events_text = []
+births_text = []
+deaths_text = []
+prepend_txt = ''
 for element in match_elements:
     element_text = element.get_text()
-    if element_text == 'Events':
-        incl = True
+    element_text = re.sub('(BC)|(AD)|(\(.*?\d+.*?\))|\[\d+\]', '', element_text.replace(str(randyear), '????'))
+    if element_text == 'Events' and curr_sec == '':
+        curr_sec = 'Events'
         continue
-    elif element_text == 'References':
-        incl = False
+    elif element_text == 'References' and curr_sec == 'Deaths':
+        curr_sec = ''
+        continue
+    elif element_text == 'Births' and curr_sec == 'Events':
+        curr_sec = 'Births'
+        continue
+    elif element_text == 'Deaths' and curr_sec == 'Births':
+        curr_sec = 'Deaths'
         continue
     elif element.name == 'h2':
+        curr_sec = ''
         continue
     if '\n' in element_text:
         continue
-    if incl:
-        clues_text.append(element_text)
-print(clues_text)
+    if curr_sec == 'Events':
+        events_text.append(element_text)
+    elif curr_sec == 'Births':
+        births_text.append('Born on this year: ' + element_text)
+    elif curr_sec == 'Deaths':
+        deaths_text.append('Died on this year: ' + element_text)
+for clue in events_text:
+    print(clue)
+for clue in births_text:
+    print(clue)
+for clue in deaths_text:
+    print(clue)
